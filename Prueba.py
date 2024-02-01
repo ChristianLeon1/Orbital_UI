f49f7b0701162b6886adb491d070acdfd9738d5f
enigmatico1 <chrisrl604@gmail.com>
2024-01-31 23:51:09 -0600

    Modulos innecesarios

--- Prueba.py
+++
@@ -1,8 +0,0 @@
-
-
-from PySide6.QtSerialPort import QSerialPortInfo 
-
-serialPortInfos = QSerialPortInfo.availablePorts()
-for portInfo in serialPortInfos:
-    print(f"{portInfo.portName()} {type(portInfo.portName())} \n")
-             
